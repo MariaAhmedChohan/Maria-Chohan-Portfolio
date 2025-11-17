544f98c86262a78d7cd5976853c09f97170b8852
MariaAhmedChohan <mariaahmedchohan@gmail.com>
2025-11-17 23:09:03 +0500

    add files

---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const About = () => {
+  return (
+    <div>
+      <motion.div
+              
+                      initial={{opacity:4, y:50}}
+                      WhileInView={{opacity:1, y:0}}
+                      transition={{duration:0.6, ease:'easeOut'}}
+                      viewport={{once: true}}
+                      id='about'
+                      className='min-h-screen items-center flex pt-20 pb-16 bg-linear-to-r from-[#064E3B] via-[#464545] to-[#064E3B]'
+                      >
+                          <div className='container mx-auto px-6 flex flex-col md:flex-row items-center justify-between'>
+                            {/* Left Side */}
+                              <div className='md:1/2 mb-10 md:mb-0'>
+                                  <h1 className='text-4xl md:text-6xl font bold mb-4'>Hi! I'am <span className='text-amber-500 font-semibold'>About</span>
+                                  </h1>
+                                  <h2 className='text-5xl md:4xl font:bold mb-4 typewriter'>Full Stack Developer</h2>
+                                  <p className='text-lg text-gray-300 mb-8'>I create stunning web Experiences with modern technologies and innovative design</p>
+                                  <div className='flex space-x-4'>
+                                    <a href="#projects" className='px-6 py-3 bg-amber-600 rounded-lg font-medium bor shadow-lg hover:bg-amber-400 transition duration-300'>View Work</a>
+                                    <a href="#contact" className='px-6 py-3 border-2 border-amber-600 rounded-lg font-medium shadow-lg  hover:bg-amber-400 transition decoration-amber-300'>Contact me</a>
+                                  </div>
+                              </div>
+              
+                           {/* Right Side */}
+                          <div className='md:w-1/2 flex justify-center'>
+                                <div className='relative w-64 h-64 md:w-80 md:h-80'>
+                                      <div className='absolute inset-0 rounded-full bg-linear-to-r from-[#064E3B] to-[#464545] opacity-70'>
+                                      <motion.img
+                                       animate={{ y: [0, -20, 0] }}
+                                       transition={{duration: 4,
+                                        repeat: Infinity,
+                                        repeatType: 'loop',
+                                        ease: 'easeInOut'
+                                       }}         
+                                      className='relative rounded-full w-64 h-64  border-amber-500 border-2  md:w-80 md:h-80 object-cover z-10 animate-float'
+                                      src={'/src/assets/maria.jpeg'} alt="profile" />
+                                      </div>
+                                </div>
+                          </div> 
+                    </div>
+                   </motion.div>
+                  
+              
+                    
+           </div>
+  )
+}
+
+export default About
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {FaXmark} from 'react-icons/fa6'
+import { useState } from 'react'
+const Navbar = () => {
+// Function showmenu
+const [showmenu, setshowmenu] = useState(false)
+
+  
+  return (
+    <nav className='fixed w-full z-50 bg-emerald-950 backdrop-blur-sm py-4 px-8 shadow-lg '>
+      <div className='container mx-auto flex justify-between items-center'>
+        <div>
+          <a href="#" className='text-4xl font-bold text-white'>
+            Maria 
+            <span className='text-amber-500'>Chohan</span>
+            </a> 
+       </div>
+       <div className='hidden md:flex space-x-10'>
+
+        <a href="#home" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+          <span>Home</span>
+          <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full'></span>
+        </a>
+
+          <a href="#about" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+          <span>About</span>
+          <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full'></span>
+        </a>
+
+          <a href="#skills" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+          <span>Skills</span>
+          <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full'></span>
+        </a>
+
+          <a href="#projects" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+          <span>Projects</span>
+          <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full'></span>
+        </a>
+
+          <a href="#contact" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+          <span>Contact</span>
+          <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-amber-500 transition-all duration-300 group-hover:w-full'></span>
+        </a>
+        
+       </div>
+     
+       {/* Mobile view */}
+         
+     <div className='md:hidden text-amber-500 font-bold text-2xl'>
+      {
+        showmenu?
+        
+        <FaXmark 
+        onClick= {()=>setshowmenu(!showmenu)}
+        className='cursor-pointer'/>:
+        <FaBars onClick= {()=>setshowmenu(!showmenu)}
+        className='cursor-pointer'/>
+      }
+
+      
+      
+      
+      
+      
+      
+                     
+                     
+               </div>       
+
+                 </div> 
+{/* Mobile menu */}
+                 {
+        showmenu && (
+              <div className='md:hidden mt-4 bg-emerald-900 h-screen rounded-lg p-4 flex flex-col space-y-4 justify-center items-center'>
+                          <a onClick= {()=>setshowmenu(!showmenu)} href="#home" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+                          <span>Home</span>
+                          </a>
+
+                          <a onClick= {()=>setshowmenu(!showmenu)} href="#home" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+                          <span>Home</span>
+                          </a>
+
+                          <a onClick= {()=>setshowmenu(!showmenu)} href="#home" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+                          <span>Home</span>
+                          </a>
+
+                          <a onClick= {()=>setshowmenu(!showmenu)} href="#home" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+                          <span>Home</span>
+                          </a>
+
+                          <a onClick= {()=>setshowmenu(!showmenu)} href="#home" className='relative text-2xl font-semibold text-white transition duration-300 hover:text-amber-500 group'>
+                          <span>Home</span>
+                          </a>
+
+          
+          
+
+                      </div>
+                  )
+      }   
+      
+
+
+    </nav>
+  )
+}
+
+export default Navbar
