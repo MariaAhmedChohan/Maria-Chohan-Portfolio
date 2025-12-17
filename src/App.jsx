9cbb0ba71d56ee7ecffe0be57354eb609f111541
MariaAhmedChohan <mariaahmedchohan@gmail.com>
2025-12-17 22:54:39 +0500

    resolve issue

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element= {<Home/>}/>
+        <Route path='/Maria-Chohan-Portfolio/' element= {<Home/>}/>
       </Routes>
       <Footer />
       
