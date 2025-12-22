27719a2cffa68fe9c865ba56cb3864aaa77e777e
MariaAhmedChohan <mariaahmedchohan@gmail.com>
2025-12-22 13:46:17 +0500

    deploy settings

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+    <HashRouter>
+        {/* Git hub is not supporting BrowserRouter for pages hosted on gh-pages. */}
+{/* <BrowserRouter> */} 
     <App />
-</BrowserRouter>
+ {/* </BrowserRouter> */}
+</HashRouter>
 )
