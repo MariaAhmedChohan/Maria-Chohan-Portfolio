3975c21570797d5024987a3449f2df4b04c2bdc5
MariaAhmedChohan <mariaahmedchohan@gmail.com>
2025-12-22 14:57:21 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
+// GitHub is not supporting BrowserRouter for pages hosted on gh-pages.
 createRoot(document.getElementById('root')).render(
-    <HashRouter base="/Maria-Portfolio/">
-        {/* Git hub is not supporting BrowserRouter for pages hosted on gh-pages. */}
-{/* <BrowserRouter> */} 
-    <App />
- {/* </BrowserRouter> */}
-</HashRouter>
+    <StrictMode>
+        <BrowserRouter basename='/Maria-Portfolio/'>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
 )
