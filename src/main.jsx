80f13eed5848aa1b4000bd654fbcd4de5347e571
MariaAhmedChohan <mariaahmedchohan@gmail.com>
2025-12-22 14:14:05 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <HashRouter base="/Mario-Portfolio-main.jsx/">
         {/* Git hub is not supporting BrowserRouter for pages hosted on gh-pages. */}
 {/* <BrowserRouter> */} 
     <App />
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: '/Mario-Portfolio/',
+  base: '/Mario-Portfolio-main.jsx /'
 })
