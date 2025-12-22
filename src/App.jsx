0a0330e27abf97690ffdbd3ee9e5442b7b46c6fe
MariaAhmedChohan <mariaahmedchohan@gmail.com>
2025-12-23 04:30:51 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element= {<Home/>}/>
+        <Route path='/about' element= {<Home/>}/>
       </Routes>
       <Footer />
     </div>
